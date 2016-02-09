aab14d5656eb82bb2a68c88acbae64e0771f77b9
Jordi PS <planadecu@gmail.com>
2016-02-09 01:58:33 +0100

    postgres jdbc config

--- src/main/java/com/pdux/tripalia/config/JpaConfig.java
+++ src/main/java/com/pdux/tripalia/config/JpaConfig.java
@@ -1,5 +1,6 @@
 package com.pdux.tripalia.config;
 
+import java.net.URISyntaxException;
 import java.util.Properties;
 
 import javax.persistence.EntityManagerFactory;
@@ -7,6 +8,7 @@ import javax.sql.DataSource;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,9 +19,9 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
 import org.springframework.util.ClassUtils;
 
+import com.heroku.sdk.jdbc.DatabaseUrl;
 import com.pdux.tripalia.Application;
 
 @Configuration
@@ -27,55 +29,70 @@ import com.pdux.tripalia.Application;
 @EnableJpaRepositories(basePackageClasses = Application.class)
 class JpaConfig {
 
-    @Value("${dataSource.driverClassName}")
-    private String driver;
-    @Value("${dataSource.url}")
-    private String url;
-    @Value("${dataSource.username}")
-    private String username;
-    @Value("${dataSource.password}")
-    private String password;
-    @Value("${hibernate.dialect}")
-    private String dialect;
-    @Value("${hibernate.hbm2ddl.auto}")
-    private String hbm2ddlAuto;
+	@Value("${dataSource.driverClassName}")
+	private String driver;
+	@Value("${dataSource.url}")
+	private String url;
+	@Value("${dataSource.username}")
+	private String username;
+	@Value("${dataSource.password}")
+	private String password;
+	@Value("${hibernate.dialect}")
+	private String dialect;
+	@Value("${hibernate.hbm2ddl.auto}")
+	private String hbm2ddlAuto;
 
-    @Bean
-    public DataSource dataSource() {
-        HikariConfig config = new HikariConfig();
-        config.setDriverClassName(driver);
-        config.setJdbcUrl(url);
-        config.setUsername(username);
-        config.setPassword(password);
-        config.addDataSourceProperty("cachePrepStmts", "true");
-        config.addDataSourceProperty("prepStmtCacheSize", "250");
-        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
-        config.addDataSourceProperty("useServerPrepStmts", "true");
+	@Bean
+	public DataSource dataSource() throws URISyntaxException {
+		HikariConfig config = new HikariConfig();
+		String databaseURL = System.getenv("DATABASE_URL");
+		config.setDriverClassName(driver);
+		if (databaseURL == null) {
+			config.setJdbcUrl(url);
+			config.setUsername(username);
+			config.setPassword(password);
+		} else {
+			DatabaseUrl databaseUrl = new DatabaseUrl(databaseURL);
+			config.setJdbcUrl(databaseUrl.jdbcUrl());
+			config.setUsername(databaseUrl.username());
+			config.setPassword(databaseUrl.password());
+		}
+		config.addDataSourceProperty("ssl", "true");
+		config.addDataSourceProperty("sslfactory", "org.postgresql.ssl.NonValidatingFactory");
+		config.addDataSourceProperty("cachePrepStmts", "true");
+		config.addDataSourceProperty("prepStmtCacheSize", "250");
+		config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+		config.addDataSourceProperty("useServerPrepStmts", "true");
 
-        return new HikariDataSource(config);
-    }
+		return new HikariDataSource(config);
+	}
 
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
-        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
-        entityManagerFactoryBean.setDataSource(dataSource);
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+			DataSource dataSource) {
+		LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+		entityManagerFactoryBean.setDataSource(dataSource);
 
-        String entities = ClassUtils.getPackageName(Application.class);
-        String converters = ClassUtils.getPackageName(Jsr310JpaConverters.class);
-        entityManagerFactoryBean.setPackagesToScan(entities, converters);
+		String entities = ClassUtils.getPackageName(Application.class);
+		String converters = ClassUtils
+				.getPackageName(Jsr310JpaConverters.class);
+		entityManagerFactoryBean.setPackagesToScan(entities, converters);
 
-        entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+		entityManagerFactoryBean
+				.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
 
-        Properties jpaProperties = new Properties();
-        jpaProperties.put(org.hibernate.cfg.Environment.DIALECT, dialect);
-        jpaProperties.put(org.hibernate.cfg.Environment.HBM2DDL_AUTO, hbm2ddlAuto);
-        entityManagerFactoryBean.setJpaProperties(jpaProperties);
+		Properties jpaProperties = new Properties();
+		jpaProperties.put(org.hibernate.cfg.Environment.DIALECT, dialect);
+		jpaProperties.put(org.hibernate.cfg.Environment.HBM2DDL_AUTO,
+				hbm2ddlAuto);
+		entityManagerFactoryBean.setJpaProperties(jpaProperties);
 
-        return entityManagerFactoryBean;
-    }
+		return entityManagerFactoryBean;
+	}
 
-    @Bean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-        return new JpaTransactionManager(entityManagerFactory);
-    }
+	@Bean
+	public PlatformTransactionManager transactionManager(
+			EntityManagerFactory entityManagerFactory) {
+		return new JpaTransactionManager(entityManagerFactory);
+	}
 }
