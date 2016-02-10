4320df3fb91049e6bf91bb44aa57dec6e5cec1be
Jordi PS <planadecu@gmail.com>
2016-02-10 01:41:38 +0100

    new relic + mongodb

--- src/main/java/com/pdux/tripalia/home/CityRepository.java
+++ src/main/java/com/pdux/tripalia/home/CityRepository.java
@@ -5,5 +5,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CityRepository extends MongoRepository<City, Long> {
-	City findOneName(String name);
 }
--- src/main/java/com/pdux/tripalia/home/HomeController.java
+++ src/main/java/com/pdux/tripalia/home/HomeController.java
@@ -3,23 +3,25 @@ package com.pdux.tripalia.home;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.mongodb.DBCollection;
 
 @Controller
 public class HomeController {
 
 	@Autowired
-	private CityRepository cityRepository;
+	private MongoTemplate mongoTemplate;
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index(Principal principal) {
-		City city = new City();
-		city.setId(123L);
-		city.setName("jordi");
-		city.setBookingUrl("dasdsa");
-		cityRepository.save(city);
+
+		DBCollection collection = mongoTemplate.getCollection("city");
+		System.out.println("db " + mongoTemplate.getDb() + " collection count "
+				+ collection.getCount());
 		return principal != null ? "home/homeSignedIn" : "home/homeNotSignedIn";
 	}
 }
